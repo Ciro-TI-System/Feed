8d17d94c2f48275f2c8885abc464307611fbbf24
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-07-21 12:32:50 -0300

    Update CSS modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
 import './styles.css';
 
 export function App() {
   return (
     <div>
-      <h1> Posts do APP </h1>
+      <Header />
       <Post
         author='Ciro Batista'
         content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae placeat quam voluptas ratione, delectus ad corrupti quis error recusandae ipsa dicta aperiam iure aliquam nostrum reiciendis nulla dignissimos perspiciatis quisquam.'
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,9 @@
-export function Header(){
-  
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <strong >Ignite Feed</strong>
+    </header>
+  );
 }
