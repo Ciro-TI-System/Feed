eb8bcfc6aeb4fcec32a542075ebff39ba5334224
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-07-21 11:18:23 -0300

    Update
    Learning about properties.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <h1> Hello World! </h1>
-      <Post />
+      <h1> Posts do APP </h1>
+      <Post
+        author='Ciro Batista'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae placeat quam voluptas ratione, delectus ad corrupti quis error recusandae ipsa dicta aperiam iure aliquam nostrum reiciendis nulla dignissimos perspiciatis quisquam.'
+      />
+      <Post
+        author='Kely Cristina'
+        content='Um novo post bem legal!'
+      />
     </div>
-
   )
 }
