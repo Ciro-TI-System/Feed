e526e5b4fb51b41dd2cf3c7608f9d57fa6c1aec8
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-07-23 13:39:47 -0300

    Update Sidebar
    Creation of the application's body structure, where the Sidebar and Post Feed are displayed, beginning of the construction and styling of the Sidebar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 import './global.css';
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author='Ciro Batista'
-        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae placeat quam voluptas ratione, delectus ad corrupti quis error recusandae ipsa dicta aperiam iure aliquam nostrum reiciendis nulla dignissimos perspiciatis quisquam.'
-      />
-      <Post
-        author='Kely Cristina'
-        content='Um novo post bem legal!'
-      />
+
+      <div className={styles.wrapper}>
+       <Sidebar />
+        <main>
+          <Post
+            author='Ciro Batista'
+            content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae placeat quam voluptas ratione, delectus ad corrupti quis error recusandae ipsa dicta aperiam iure aliquam nostrum reiciendis nulla dignissimos perspiciatis quisquam.'
+          />
+          <Post
+            author='Kely Cristina'
+            content='Um novo post bem legal!'
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
