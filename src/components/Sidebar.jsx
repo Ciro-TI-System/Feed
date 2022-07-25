764f2f2cd3485f7214ff23a09e41d7d9e1149178
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-07-25 13:24:28 -0300

    Update Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,29 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
-  return(
+  return (
     <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
 
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/78507494?v=4"
+        />
+        <strong>Ciro Batista</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine />
+          Editar seu perfil
+        </a>
+      </footer>
     </aside>
   );
 }
